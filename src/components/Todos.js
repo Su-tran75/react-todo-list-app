08972be5b0aefb2397dcaa6ef28d1930bf1468ef
su-tran75 <su-tran@hotmail.fr>
2021-11-25 12:03:47 +0800

    connect input and add button with this state and methods

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,8 +2,33 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { addTodos } from "../redux/reducer";
 
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
 function Todos(props) {
+  console.log("Todos -> props", props);
   const [todo, setTodo] = useState("");
+
+  const add = () => {
+    if (todo === "") {
+      alert("Input is empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 10000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
 
   const handleChange = (e) => {
     setTodo(e.target.value);
@@ -17,20 +42,18 @@ function Todos(props) {
         onChange={handleChange}
         value={todo}
       />
-      <button className="add-btn">Add</button>
+      <button className="add-btn" onClick={add}>
+        Add
+      </button>
       <br />
+      <ul>
+        {props.todos.length > 0 &&
+          props.todos.map((item) => {
+            return <li key={item.id}>{item.item}</li>;
+          })}
+      </ul>
     </div>
   );
 }
-const mapStateToProps = (state) => {
-  return {
-    todos: state,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {
-    addTodo: (obj) => dispatch(addTodos(obj)),
-  };
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Todos);
